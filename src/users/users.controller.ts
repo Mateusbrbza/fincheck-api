a9b623126587e87d54b883b4e4702e06f3b92cae
mateusbrbza <mateus.c.barboza@gmail.com>
2023-09-09 23:15:26 -0300

    feat: iniciando o users modules

---
+++ src/users/users.controller.ts
@@ -0,0 +1,20 @@
+import {
+  Controller,
+  Post,
+  Body,
+  UsePipes,
+  ValidationPipe,
+} from '@nestjs/common';
+import { UsersService } from './users.service';
+import { CreateUserDto } from './dto/create-user.dto';
+
+@Controller('users')
+export class UsersController {
+  constructor(private readonly usersService: UsersService) {}
+
+  @Post()
+  @UsePipes(new ValidationPipe())
+  create(@Body() createUserDto: CreateUserDto) {
+    return this.usersService.create(createUserDto);
+  }
+}
