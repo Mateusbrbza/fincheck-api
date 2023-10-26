557c86876734522cc6bd57ae3f5146e5ee2d6f8d
mateusbrbza <mateus.c.barboza@gmail.com>
2023-10-26 17:34:33 -0300

    feat: starting transactions module

---
+++ src/modules/transactions/dto/create-transaction.dto.ts
@@ -0,0 +1,33 @@
+import {
+  IsDateString,
+  IsNotEmpty,
+  IsNumber,
+  IsPositive,
+  IsString,
+  IsUUID,
+} from 'class-validator';
+
+export class CreateTransactionDto {
+  @IsString()
+  @IsNotEmpty()
+  @IsUUID()
+  bankAccountId: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @IsUUID()
+  categoryId: string;
+
+  @IsString()
+  @IsNotEmpty()
+  name: string;
+
+  @IsNumber()
+  @IsNotEmpty()
+  @IsPositive()
+  value: number;
+
+  @IsNotEmpty()
+  @IsDateString()
+  date: string;
+}
