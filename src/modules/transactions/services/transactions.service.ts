397a012f03128b162ea407fcbd9390c04dd47850
mateusbrbza <mateus.c.barboza@gmail.com>
2024-03-02 01:36:18 -0300

    findAllByUserId update

--- src/modules/transactions/services/transactions.service.ts
+++ src/modules/transactions/services/transactions.service.ts
@@ -60,6 +60,15 @@ export class TransactionsService {
           lt: new Date(Date.UTC(filters.year, filters.month + 1)),
         },
       },
+      include: {
+        category: {
+          select: {
+            id: true,
+            name: true,
+            icon: true,
+          },
+        },
+      },
     });
   }
 
